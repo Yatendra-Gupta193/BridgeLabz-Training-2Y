51b0f87af22be3b522a7b6300b955cc3124dfb93
Yatendra kumar gupta <guptajikrish12@gmail.com>
2025-08-25 23:50:04 +0530

    PowerWhile.java

---
+++ control-flow/level 2/PowerWhile.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PowerWhile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a base number: ");
+        int number = scanner.nextInt();
+
+        System.out.print("Enter the power: ");
+        int power = scanner.nextInt();
+
+        int result = 1;
+        int counter = 1;
+
+        while (counter <= power) {
+            result *= number;
+            counter++;
+        }
+
+        System.out.println("Result: " + result);
+
+        scanner.close();
+    }
+}
