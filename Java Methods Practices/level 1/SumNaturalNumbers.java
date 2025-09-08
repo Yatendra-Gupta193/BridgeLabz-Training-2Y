d940ffcc982e10b3a0811ffbae5691a6adb91d94
Yatendra kumar gupta <guptajikrish12@gmail.com>
2025-09-08 22:51:33 +0530

    SumNaturalNumbers.java

---
+++ Java Methods Practices/level 1/SumNaturalNumbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumNaturalNumbers {
+    public static int sumNaturalNumbers(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }  
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+        
+        int sum = sumNaturalNumbers(n);
+        System.out.println("Sum of first " + n + " natural numbers: " + sum);
+        
+        scanner.close();
+    }
+}
