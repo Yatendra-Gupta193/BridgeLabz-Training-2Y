3ced6e1aa39dd828552936deb94df8f47d1e5691
Yatendra kumar gupta <guptajikrish12@gmail.com>
2025-09-08 23:04:02 +0530

    WindChillCalculator.java

---
+++ Java Methods Practices/level 1/WindChillCalculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class WindChillCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter temperature (in Fahrenheit): ");
+        double temp = sc.nextDouble();
+        System.out.print("Enter wind speed (in mph): ");
+        double windSpeed = sc.nextDouble();
+        
+        double windChill = 35.74 + 0.6215 * temp + (0.4275 * temp - 35.75) * Math.pow(windSpeed, 0.16);
+        
+        System.out.printf("Wind chill temperature: %.2f°F\n", windChill);
+        
+        sc.close();
+    }
+}
