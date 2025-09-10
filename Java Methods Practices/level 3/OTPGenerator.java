5b6fca10c15a1462f8c4278c19e03d1a8cbc9e12
Yatendra kumar gupta <guptajikrish12@gmail.com>
2025-09-11 00:46:34 +0530

    OTPGenerator.java

---
+++ Java Methods Practices/level 3/OTPGenerator.java
@@ -0,0 +1,25 @@
+public class OTPGenerator {  
+    public static void main(String[] args) {
+        int[] otps = new int[10];      
+        for (int i = 0; i < 10; i++) {
+            otps[i] = generateOTP();
+        }      
+        System.out.println("Generated OTPs:");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("OTP " + (i+1) + ": " + otps[i]);
+        }
+        
+        System.out.println("All OTPs unique: " + areUnique(otps));
+    } 
+    public static int generateOTP() {
+        return (int)(Math.random() * 900000) + 100000;
+    }    
+    public static boolean areUnique(int[] otps) {
+        for (int i = 0; i < otps.length; i++) {
+            for (int j = i + 1; j < otps.length; j++) {
+                if (otps[i] == otps[j]) return false;
+            }
+        }
+        return true;
+    }
+}
