b856232b4ca5d763d0b5a5e3e48027e9bb02efd3
Yatendra kumar gupta <guptajikrish12@gmail.com>
2025-09-11 11:04:12 +0530

    ToggleCase.java

---
+++ Java String Practice Problems/ToggleCase.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ToggleCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        StringBuilder result = new StringBuilder();
+        for (char ch : str.toCharArray()) {
+            if (Character.isUpperCase(ch)) result.append(Character.toLowerCase(ch));
+            else if (Character.isLowerCase(ch)) result.append(Character.toUpperCase(ch));
+            else result.append(ch);
+        }
+        System.out.println(result.toString());
+    }
+}
