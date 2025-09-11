5b9ee10847ceb042b5e35252a49515fd29380b55
Yatendra kumar gupta <guptajikrish12@gmail.com>
2025-09-11 11:01:24 +0530

    RemoveDuplicates.java

---
+++ Java String Practice Problems/RemoveDuplicates.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (result.indexOf(str.charAt(i)) == -1) result += str.charAt(i);
+        }
+        System.out.println(result);
+    }
+}
