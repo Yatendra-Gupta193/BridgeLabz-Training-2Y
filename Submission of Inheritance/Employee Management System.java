087f4adf5335e29afcede01f3bc68b118196f170
Yatendra kumar gupta <guptajikrish12@gmail.com>
2025-09-24 01:56:30 +0530

    Employee Management System.java

---
+++ Submission of Inheritance/Employee Management System.java
@@ -0,0 +1,63 @@
+class Employee {
+    String name;
+    int id;
+    double salary;
+
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Salary: $" + salary);
+    }
+}
+
+class Manager extends Employee {
+    int teamSize;
+
+    public Manager(String name, int id, double salary, int teamSize) {
+        super(name, id, salary);
+        this.teamSize = teamSize;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Team Size: " + teamSize);
+        System.out.println("Role: Manager");
+    }
+}
+
+class Developer extends Employee {
+    String programmingLanguage;
+
+    public Developer(String name, int id, double salary, String programmingLanguage) {
+        super(name, id, salary);
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Programming Language: " + programmingLanguage);
+        System.out.println("Role: Developer");
+    }
+}
+class Intern extends Employee {
+    String university;
+
+    public Intern(String name, int id, double salary, String university) {
+        super(name, id, salary);
+        this.university = university;
+    }
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("University: " + university);
+        System.out.println("Role: Intern");
+    }
+}
