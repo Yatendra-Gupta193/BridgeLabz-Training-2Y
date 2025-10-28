9c55e3505057803d9c1e652007be8fe1b187d5b2
Yatendra kumar gupta <guptajikrish12@gmail.com>
2025-10-28 18:50:30 +0530

    Hospital Patient ID Printing.java

---
+++ Lambda Expressions/Method References/Hospital Patient ID Printing.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class HospitalPatientID {
+    public static void main(String[] args) {
+        List<String> patientIDs = Arrays.asList("P001", "P002", "P003", "P004");
+        
+        patientIDs.forEach(System.out::println);
+    }
+}
