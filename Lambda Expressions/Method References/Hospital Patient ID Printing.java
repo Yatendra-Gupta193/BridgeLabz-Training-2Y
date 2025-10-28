64aee28a039d310365b8aa541ad345ddd2435164
Yatendra kumar gupta <guptajikrish12@gmail.com>
2025-10-28 18:47:34 +0530

    Delete Lambda Expressions/Method References directory

--- Lambda Expressions/Method References/Hospital Patient ID Printing.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class HospitalPatientID {
-    public static void main(String[] args) {
-        List<String> patientIDs = Arrays.asList("P001", "P002", "P003", "P004");
-        
-        patientIDs.forEach(System.out::println);
-    }
-}
