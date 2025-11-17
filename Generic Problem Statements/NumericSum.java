644b1bd38181eff94dbb0ddcd6e14f7eec8d0883
Yatendra kumar gupta <guptajikrish12@gmail.com>
2025-11-17 23:41:18 +0530

    NumericSum.java

---
+++ Generic Problem Statements/NumericSum.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class NumericSum {
+
+    static double sumNumbers(List<? extends Number> list) {
+        double sum = 0;
+
+        for (Number n : list) {
+            sum += n.doubleValue();
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 3);
+        List<Double> doubleList = Arrays.asList(1.5, 2.5, 3.5);
+
+        System.out.println(sumNumbers(intList));
+        System.out.println(sumNumbers(doubleList));
+    }
+}
